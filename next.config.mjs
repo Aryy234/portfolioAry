d4673f3f484f7c1efce2cc9ab6218e2dc95a1a19
Aryy234 <amelizalde@uce.edu.ec>
2025-03-08 00:22:15 -0500

    modificacion de next

--- next.config.mjs
+++ next.config.mjs
@@ -1,18 +1,37 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   eslint: {
+    // Ignorar errores de ESLint durante la construcción para evitar bloqueos
     ignoreDuringBuilds: true,
   },
   typescript: {
+    // Ignorar errores de TypeScript durante la construcción para avanzar sin interrupciones
     ignoreBuildErrors: true,
   },
   images: {
+    // No optimizar imágenes si hay problemas relacionados con la optimización de imágenes
     unoptimized: true,
   },
   experimental: {
-    webpackBuildWorker: false, // Desactivado para evitar problemas
+    // Desactivar funciones experimentales para evitar incompatibilidades
+    webpackBuildWorker: false,
     parallelServerBuildTraces: false,
     parallelServerCompiles: false,
+  },
+  webpack: (config) => {
+    // Configuración adicional para evitar problemas con Webpack
+    config.optimization = {
+      ...config.optimization,
+      splitChunks: {
+        maxSize: 200000, // Limitar el tamaño máximo de los chunks
+      },
+    };
+
+    config.infrastructureLogging = {
+      debug: false, // Puedes cambiar esto a true para obtener logs más detallados si necesitas depurar
+    };
+
+    return config;
   },
 };
 
